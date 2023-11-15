f2242ab4a16ebb5a0e858f331af7082f6e0ac196
Amos-jaf <araunahamos@gmail.com>
2023-11-15 04:45:26 +0000

    modify functions2.c file

--- functions2.c
+++ functions2.c
@@ -9,6 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ * Description: pointer modificagtions
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
